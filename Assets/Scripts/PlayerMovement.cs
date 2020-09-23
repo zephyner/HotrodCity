1cbbac01ef7abeaab54efbc25b8fd55f99b3cc72
Keshy <keshavan.easwaradeva@hotmail.com>
2020-09-23 13:40:55 -0400

    Added A Car

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float speed = 10;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
+    }
+}
