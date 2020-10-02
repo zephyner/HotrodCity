53ec9b275e36565876a3340951845ada2b9a8f8f
zephyner <vanjasonn@gmail.com>
2020-10-02 03:11:47 -0400

    moar network WIP

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : NetworkBehaviour
+{
+    
+        void Start()
+    {
+        
+    }
+
+    [Client]
+    void Update()
+    {
+        // has authority currently checked in networktransform on PCar prefab
+        if (!hasAuthority)
+        {
+            return;
+        }
+    }
+}
